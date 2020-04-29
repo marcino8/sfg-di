2bd41df9b4cb0e04297242b7db87ade7588ac986
marcino8 <mklimczak7@gmail.com>
2020-04-29 13:25:55 +0200

    Corrected minor errors for PetController implementation

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,13 +1,19 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 
 /**
  * Created by jt on 12/28/19.
  */
+@Controller
 public class PetController {
 
-    private final PetService petService;
+    @Qualifier("petService")
+    @Autowired
+    public PetService petService;
 
     public String whichPetIsTheBest(){
         return petService.getPetType();
